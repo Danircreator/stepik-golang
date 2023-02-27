0c7e66aaa678fe0e60a2e7a7ab4f674d40293b8a
danirdanir <danirdanir@gmail.com>
2023-02-27 20:03:05 +0500

    First lesson

--- 1-1.go
+++ 1-1.go
@@ -1,4 +1,13 @@
-fgkdfjgkldfjg d
-gdfk gdfkg df
- gdfkgdf
- gdfk
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+
+	fmt.Scanln(&a)
+
+	fmt.Println("The next number for the number", a, "is", a+1)
+	fmt.Println("The previous number for the number", a, "is", a-1)
+
+}
