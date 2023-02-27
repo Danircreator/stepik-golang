5c59b401125605d51148b3359988d7cc2283384b
danirdanir <danirdanir@gmail.com>
2023-02-27 20:29:09 +0500

    Success

--- 1-1.go
+++ 1-1.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var a int
+	var n int
+	fmt.Scan(&n)
 
-	fmt.Scanln(&a)
+	next := n + 1
+	prev := n - 1
 
-	fmt.Println("The next number for the number", a, "is", a+1)
-	fmt.Println("The previous number for the number", a, "is", a-1)
-
+	fmt.Printf("The next number for the number %d is %d.\n", n, next)
+	fmt.Printf("The previous number for the number %d is %d.", n, prev)
 }
