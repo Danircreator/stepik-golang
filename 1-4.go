5490633181bddfc2d300de79b595e44de88ba5bc
danirdanir <danirdanir@gmail.com>
2023-03-06 20:28:07 +0500

    not success

---
+++ 1-4.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var l int = 109
+	var v int
+	var t int
+	var result int
+
+	fmt.Scanln(&v)
+	fmt.Scanln(&t)
+
+	result = (v * t) - l
+
+	fmt.Println(result)
+
+}
